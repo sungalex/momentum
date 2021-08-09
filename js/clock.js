78744bf5bcdfd429c899bf81823ec9aedc13e633
sungalex <cloudallera@gmail.com>
2021-08-09 10:44:00 +0900

    append Date

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,19 @@
-const clock = document.querySelector('#clock');
+const dateSpan = document.querySelector("#clock span:first-child");
+const timeSpan = document.querySelector("#clock span:last-child");
 
 // setInterval() : 정해진 시간이 경과할 때마다 Callback 함수를 호출해주는 내장 함수
 // setTimeout() : 일정한 시간이 지난 후 한번만 Callback 함수를 호출하는 함수
 function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const today = new Date();
+  const year = String(today.getFullYear());
+  const month = String(today.getMonth()).padStart(2, '0');
+  const date = String(today.getDate()).padStart(2, '0');
+  const hours = String(today.getHours()).padStart(2, '0');
+  const minutes = String(today.getMinutes()).padStart(2, '0');
+  const seconds = String(today.getSeconds()).padStart(2, '0');
+
+  dateSpan.innerText = `${year}-${month}-${date}`;
+  timeSpan.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
